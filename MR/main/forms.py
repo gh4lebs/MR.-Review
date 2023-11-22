c80266378fd19ce9737472e4b6f59d62134d2e81
gh4lebs <ghalebalsoufi@gmail.com>
2023-11-22 17:04:39 +0300

    Initial working commit

---
+++ MR/main/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+from .models import *
+from django.urls import reverse_lazy
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
+
+
+# movie add form
+
+
+class MovieForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_action = reverse_lazy("index")
+        self.helper.form_method = 'POST'
+
+    class Meta:
+        model = Movie
+        fields = ('name', 'director', 'cast',
+                  'description', 'release_date', 'image')
